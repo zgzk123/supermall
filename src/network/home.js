ae3d00ef80bed830d3fbe01e39a038cec3869314
zgzk123 <1938972351@qq.com>
2020-03-25 08:31:55 +0800

    第二次

---
+++ src/network/home.js
@@ -0,0 +1,6 @@
+import {request} from "./request";
+export function getHomeMultdata() {
+  return request({
+    url:'/home/multidata'
+  })
+}
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <swiper>
+      <swiper-item v-for="item in banners">
+        <a :href="item.link">
+          <img :src="item.image"/>
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+  export default {
+    name: "home-swiper",
+    props:{
+      banners:{
+        type:Array,
+        defaults(){
+          return[]
+        }
+      }
+    },
+    components:{
+      Swiper,
+      SwiperItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
